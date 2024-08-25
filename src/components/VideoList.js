726f3e1375fb446156b7c6ddf4566090ae8a259f
Gladstone Rose <gladstonerose@hotmail.com>
2024-08-25 00:00:45 -0500

    modify VideoList.js to add 3 more videos

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -6,6 +6,9 @@ const VideoList = () => {
     'M7lc1UVf-VE', // Replace with actual YouTube video IDs
     'dQw4w9WgXcQ',
     '3JZ_D3ELwOQ',
+    'ScMzIvxBSi4', // New video ID
+    'E7wJTI-1dvQ', // New video ID
+    'kxopViU98Xo', // New video ID
   ];
 
   return (
